adc6061ca075631f938f6f737825239cb15f232b
Nate <minecraftcrusher100@gmail.com>
2023-01-14 14:54:20 -0600

    Add constants file

---
+++ src/main/Const.java
@@ -0,0 +1,6 @@
+public class Const {
+    /* Use this class to define **ALL** constants used in your code. */
+    public final static int EXAMPLE_ARBITRARY_CONSTANT = 123456789;
+    
+    public Const() {}
+}
