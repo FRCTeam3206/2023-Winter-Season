64a20105cfdfed3e4e63196a70da5673113dc528
CREATE-02 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-02-04 12:06:10 -0600

    Add drivetrain to robot container (tested)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Drivetrain;
 
 public class RobotContainer {
+    Drivetrain drive = new Drivetrain();
   public RobotContainer() {
     configureBindings();
   }
