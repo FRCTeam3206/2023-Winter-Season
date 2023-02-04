b9ab2b2b5b2f8cdb3245609d472e1bfa3d9860ff
CREATE-01 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-02-04 10:51:20 -0600

    Move Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,9 @@
-public class Const {
+package frc.robot;
+
+public class Constants {
     /* Use this class to define **ALL** constants used in your code. */
     public final static int EXAMPLE_ARBITRARY_CONSTANT = 123456789;
-    
-    public Const() {}
+
+    public Constants() {
+    }
 }
