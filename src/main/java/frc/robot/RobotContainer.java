c9b1d623b4381529cfd523b18f851f27499c71bf
CREATE-01 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-02-06 19:21:53 -0600

    Added Compressor Object

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
@@ -21,10 +23,12 @@ public class RobotContainer {
   Claw claw;
   Arm arm;
   Intake intake;
+  Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
   public RobotContainer() {
     configureBindings();
     this.claw=new Claw();
     this.intake=new Intake();
+    pcmCompressor.enableDigital();
   }
 
   private void configureBindings() {
