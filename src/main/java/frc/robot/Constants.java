31ec3f30bfb8b5cd9620309df8750aa23ea95899
CREATE-01 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-02-06 18:06:53 -0600

    Added Intake Subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
 public class Constants {
-    
+    public static final class Ports{
+        public static final int INTAKE_MOTOR=5;
+    }    
 }
---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.*;
+public class Intake extends SubsystemBase{
+    VictorSPX intakeMotor=new VictorSPX(Ports.INTAKE_MOTOR);
+    public Intake(){}
+    public void runIntake(){
+        intakeMotor.set(ControlMode.PercentOutput, 1);
+    }
+}
