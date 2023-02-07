600f9e14208cc3a4b78ac7d909172a9a46d0d46f
CREATE-01 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-02-06 18:12:14 -0600

    Added Claw Subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,5 +3,7 @@ package frc.robot;
 public class Constants {
     public static final class Ports{
         public static final int INTAKE_MOTOR=5;
+        public static final int CLAW_MASTER_PISTON = 0;
+        public static final int CLAW_SECOND_PISTON = 1;
     }    
 }
