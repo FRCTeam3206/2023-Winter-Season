48db2077399af1bafd341548f53c0bf70544b5cd
CREATE-01 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-02-11 10:58:29 -0600

    Revert Robot and RobotContainer with auton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Gyro;
 
@@ -15,5 +17,10 @@ public class RobotContainer {
         configureBindings();
     }
 
-    private void configureBindings() {}
+    private void configureBindings() {
+    }
+
+    public Command getAutonomousCommand() {
+        return Commands.print("No autonomous configured.");
+    }
 }
