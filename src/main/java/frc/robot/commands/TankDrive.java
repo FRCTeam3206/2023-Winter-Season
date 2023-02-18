349d9419abaca6b8f6090698a0bed02ae1e3589a
CREATE-02 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-02-18 10:09:13 -0600

    Add tank drive command

---
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class TankDrive extends CommandBase {
+    private final Drivetrain m_drive;
+    private final Supplier<Double> leftSupplier;
+    private final Supplier<Double> rightSupplier;
+
+    public TankDrive(Drivetrain subsystem, Supplier<Double> leftForwardPower, Supplier<Double> rightForwardPower) {
+        this.m_drive = subsystem;
+        this.leftSupplier = leftForwardPower;
+        this.rightSupplier = rightForwardPower;
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        this.m_drive.tankDrive(this.leftSupplier.get(), this.rightSupplier.get());
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+}
