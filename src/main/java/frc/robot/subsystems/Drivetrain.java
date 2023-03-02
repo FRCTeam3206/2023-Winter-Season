697757aaaf4a7c733283ac0f37b99a29bb2e3adc
CREATE-04 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-03-01 19:44:33 -0600

    remove old encoder import

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Encoder;
