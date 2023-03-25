19740eae45ab8762df0ff2bcbf3ee0702ff087f0
CREATE-02 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-03-25 11:04:57 -0500

    Absolute Encoder on Arm, only output now

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -7,7 +7,9 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 
@@ -16,13 +18,13 @@ import static frc.robot.Constants.*;
 public class Arm extends SubsystemBase {
     CANSparkMax elbow = new CANSparkMax(CANIDs.ARM_ELBOW, MotorType.kBrushless);
     VictorSPX telescope = new VictorSPX(CANIDs.ARM_TELE);
-    Encoder elbowEncoder = new Encoder(Encoders.ELBOW_A, Encoders.ELBOW_B, false);
+    DutyCycleEncoder elbowEncoder = new DutyCycleEncoder(Encoders.ELBOW_A);
     PIDController elbowPID = new PIDController(ArmConstants.elbowKp, ArmConstants.elbowKi, ArmConstants.elbowKd);
     AnalogPotentiometer teleEncoder = new AnalogPotentiometer(0, ArmConstants.ARM_SMALL, ArmConstants.ARM_BIG);
     PIDController telePID = new PIDController(ArmConstants.teleKp, ArmConstants.teleKi, ArmConstants.teleKd);
 
     public Arm() {
-        elbowEncoder.setDistancePerPulse(Math.PI * 2 / 1024);
+        elbowEncoder.setDistancePerRotation(2 * Math.PI);
         elbowPID.setSetpoint(ArmConstants.ARM_INITAL_ANGLE);
         telePID.setSetpoint(ArmConstants.ARM_SMALL);
     }
@@ -36,9 +38,12 @@ public class Arm extends SubsystemBase {
     }
 
     public void periodic() {
-        elbow.set(elbowPID.calculate(elbowEncoder.getDistance() + ArmConstants.ARM_INITAL_ANGLE));
-        telescope.set(VictorSPXControlMode.PercentOutput,
-                telePID.calculate(teleEncoder.get() + ArmConstants.ARM_SMALL));
+        // elbow.set(elbowPID.calculate(elbowEncoder.getDistance() +
+        // ArmConstants.ARM_INITAL_ANGLE));
+        // telescope.set(VictorSPXControlMode.PercentOutput,
+        // telePID.calculate(teleEncoder.get() + ArmConstants.ARM_SMALL));
+        SmartDashboard.putNumber("Elbow Position", elbowEncoder.getDistance());
+        SmartDashboard.putNumber("Tele Position", teleEncoder.get());
     }
 
 }
