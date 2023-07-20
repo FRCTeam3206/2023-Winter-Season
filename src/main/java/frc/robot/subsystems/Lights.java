57c96a94eeb6e319e058e72c660ed20316a0e002
Blue <unfixedsoup@proton.me>
2023-07-20 18:29:25 -0500

    fixed lights file

---
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.SubsystemBase;
+
+public class Lights extends SubsystemBase {
+
+  private AdressableLED m_led; 
+  private AddressableLEDBuffer m_ledBuffer;
+
+  public Lights() {
+    // PWM port 0
+    // Must be a PWM header, not MXP or DIO
+    m_led = new AddressableLED(0);
+
+    // Reuse buffer
+    // Default to a length of 60, start empty output
+    // Length is expensive to set, so only set it once, then just update data
+    m_ledBuffer = new AddressableLEDBuffer(150);
+    m_led.setLength(m_ledBuffer.getLength());
+
+    // Set the data
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+  }
+
+  @Override
+  public void periodic() {
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for red
+      m_ledBuffer.setRGB(i, 0, 0, 150);
+    }
+      m_led.setData(m_ledBuffer);
+  }
+
+}
+
